69e7d60324a6bcc95a914199b5c3a8ac630ee3b9
AleCorro <91075447+AleCorro@users.noreply.github.com>
2021-10-21 19:45:35 +0200

    Initial Commit

---
+++ src/EJERCICIO42.java
@@ -0,0 +1,18 @@
+public class EJERCICIO42 {
+
+    public static void main(String [] args) {
+
+        int numero = 0;
+        int i = 0;
+
+        System.out.println("Los 50 primeros numeros son:");
+
+        while (i < 50) {
+            if (numero >= 0) {
+                System.out.println(numero);
+             i++;
+            }
+            numero++;
+        }
+    }
+}
