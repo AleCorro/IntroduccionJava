52cdaee3fecdb8a88e181f3e8a8ca3ec6cbf3fa4
AleCorro <91075447+AleCorro@users.noreply.github.com>
2021-10-29 00:19:01 +0200

    Initial Commit

---
+++ src/EJERCICIO51.java
@@ -0,0 +1,14 @@
+public class EJERCICIO51 {
+
+    public static void main(String[] args) {
+
+
+        int[] numeros = new int[]{100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Elemento en el indice " + (i + 1) + " :" + numeros[i]);
+
+        }
+    }
+}
